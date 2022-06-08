ac0394b70469eeb2932e2f7d8827de84a69445e1
SuzBarnes <SuzBarnes@github.com>
2022-06-08 15:20:36 +0100

    objects update

--- src/objects.js
+++ src/objects.js
@@ -1,41 +1,56 @@
 const createPerson = (name, age) => {
-  // your code here
+return{
+  name: name,
+  age: age,
+}
 };
 
 const getName = object => {
-  // your code here
+
+  return object.name;
+
 };
 
 const getProperty = (property, object) => {
-  // your code here
+return object[property];
 };
 
 const hasProperty = (property, object) => {
-  // your code here
+ return object.hasOwnProperty(property);
 };
 
 const isOver65 = person => {
-  // your code here
+ return person.age>65;
+
 };
 
 const getAges = people => {
-  // your code here
+  return people.map(person=> (person.age));
 };
 
 const findByName = (name, people) => {
-  // your code here
+  return people.find(findByName => findByName.name === name);
 };
 
 const findHondas = cars => {
-  // your code here
+  return cars.filter(findHondas=> findHondas.manufacturer === "Honda");
 };
 
 const averageAge = people => {
-  // your code here
+  const sumAge = people.reduce((personAge, currentPerson) =>{
+    return personAge + currentPerson.age;
+  }, 0);
+  return sumAge/people.length;
 };
 
 const createTalkingPerson = (name, age) => {
-  // your code here
+ return {
+   name: name,
+  age: age,
+  introduce: strangersName => {
+    return `Hi ${strangersName}, my name is ${name} and I am ${age}!`;
+}
+ };
 };
 
 module.exports = {
